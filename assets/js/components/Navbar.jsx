9483a08d16ee8413dd279283266810b459a3aeef
badro73 <51475140+badro73@users.noreply.github.com>
2020-09-01 11:08:42 +0200

    correction navlik

--- assets/js/components/Navbar.jsx
+++ assets/js/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {NavLink } from 'react-router-dom';
 
 
 const Navbar=({logout,isAuthenticated,onLogin,history})=>{
@@ -15,7 +15,7 @@ const deconnected=()=>{
 return (
 
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-  <Link className="navbar-brand" to="/">Freelance</Link>
+  <NavLink  className="navbar-brand" to="/">Freelance</NavLink >
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
@@ -23,10 +23,10 @@ return (
   <div className="collapse navbar-collapse" id="navbarColor01">
     <ul className="navbar-nav mr-auto">
       <li className="nav-item">
-        <Link className="nav-link" to="/invoices">Factures</Link>
+        <NavLink  className="nav-link" to="/invoices">Factures</NavLink >
       </li>
       <li className="nav-item">
-        <Link className="nav-link" to="/costumers">Clients</Link>
+        <NavLink  className="nav-link" to="/costumers">Clients</NavLink >
       </li>
     </ul>
     
@@ -36,7 +36,7 @@ return (
         <a className="nav nav-link">inscription</a>
       </li>
       <li className="nav-item">
-        <Link className="btn btn-success" to="/connexion" >Connexion</Link>
+        <NavLink  className="btn btn-success" to="/connexion" >Connexion</NavLink >
       </li>
       </> 
       }
